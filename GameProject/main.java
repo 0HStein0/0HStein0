fae0c2236e2cc00994d60fe2172a2daa234b7276
0HStein0 <stein.51@wright.edu>
2025-04-19 20:37:21 -0400

    got rid of mazeGame and moved it to main.java, added Maze file

--- GameProject/main.java
+++ GameProject/main.java
@@ -1,8 +1,22 @@
 package GameProject;
 
+import javax.swing.*;
+
 public class main {
     public static void main(String[] args) {
-        mazeGame game = new mazeGame();
-        game.startGame();
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Maze Game");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(300, 500);
+            frame.setLocationRelativeTo(null);
+
+            Maze maze = new Maze(5, 3);
+            MazePanel mazePanel = new MazePanel(maze);
+            frame.add(mazePanel);
+
+            frame.addKeyListener(mazePanel.getKeyHandler());
+            frame.setFocusable(true);
+            frame.setVisible(true);
+        });
     }
 }
